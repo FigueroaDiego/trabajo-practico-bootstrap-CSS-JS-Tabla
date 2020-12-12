0d1dab0317711c9a8ffeddb15a6b1820f5f56ed4
diego_07 <diego_33_04@hotmail.com>
2020-12-11 21:28:03 -0300

    codigo ventana modal

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-$(document).ready(function() {
-  var table = $('#example').DataTable();
-   
-  $('#example tbody').on('click', 'tr', function () {
-      var data = table.row( this ).data();
-      alert( 'You clicked on '+data[0]+'\'s row' );
-  } );
-} );
+$("#btnNuevo").click(function(){
+alert("nuevo");
+});
