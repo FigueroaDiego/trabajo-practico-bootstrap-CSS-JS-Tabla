3e716d61e446df734a7d4d8b31dcbc1e3e8ae1e3
diego_07 <diego_33_04@hotmail.com>
2020-12-14 18:07:44 -0300

    creacion de funcionalidad

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
-$("#btnNuevo").click(function(){
-alert("nuevo");
+//Inicializando la documentacion de la tabla de datos
+$(document).ready(function(){
+    tablaPersonas = $("#tabla-productos").DataTable({
+        "columnDefs": [{
+        "targets": -1,
+        "data": null,
+        "defaultContent":"<div class='text-center'><div class='btn-group'><button class='btn btn-primary btnEditar'>Editar</button><button class='btn btn-danger btnBorrar'>Borrar</button></div></div>"
+        }],
+        
+        //Cambio de lenguaje de la tabla
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sSearch": "Buscar:",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast":"Último",
+                "sNext":"Siguiente",
+                "sPrevious": "Anterior"
+             },
+             "sProcessing":"Procesando...",
+        }
+    });
+
+    $("#btnNuevo").click(function(){
+        $("#formProductos").trigger("reset");
+        $(".modal-header").css("background-color", "#28a745");
+        $(".modal-header").css("color", "white");
+        $(".modal-title").text("Nueva Persona");            
+        $("#modalCRUD").modal("show");        
+        id=null;
+        opcion = 1; //alta       
+    });
+
 });
+
