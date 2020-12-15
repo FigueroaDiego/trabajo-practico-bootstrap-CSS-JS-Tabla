2e6f60579eedbb091d54ddaa5191841de9dca2ce
diego_07 <diego_33_04@hotmail.com>
2020-12-15 18:32:18 -0300

    modificacion tabla

--- js/main.js
+++ js/main.js
@@ -1,39 +1,35 @@
-//Inicializando la documentacion de la tabla de datos
-$(document).ready(function(){
-    tablaPersonas = $("#tabla-productos").DataTable({
-        "columnDefs": [{
-        "targets": -1,
-        "data": null,
-        "defaultContent":"<div class='text-center'><div class='btn-group'><button class='btn btn-primary btnEditar'>Editar</button><button class='btn btn-danger btnBorrar'>Borrar</button></div></div>"
-        }],
-        
-        //Cambio de lenguaje de la tabla
-        "language": {
-            "lengthMenu": "Mostrar _MENU_ registros",
-            "zeroRecords": "No se encontraron resultados",
-            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-            "sSearch": "Buscar:",
-            "oPaginate": {
-                "sFirst": "Primero",
-                "sLast":"Último",
-                "sNext":"Siguiente",
-                "sPrevious": "Anterior"
-             },
-             "sProcessing":"Procesando...",
-        }
-    });
 
-    $("#btnNuevo").click(function(){
-        $("#formProductos").trigger("reset");
-        $(".modal-header").css("background-color", "#28a745");
-        $(".modal-header").css("color", "white");
-        $(".modal-title").text("Nueva Persona");            
-        $("#modalCRUD").modal("show");        
-        id=null;
-        opcion = 1; //alta       
-    });
 
-});
+let table = document.getElementById("tablaBody");
 
+
+function traer(){    
+    fetch('/tabla.json')
+       .then(res=> res.json())
+       .then(datos => {
+           tabla(datos)
+       })
+}
+
+function tabla(datos){
+    table.innerHTML = ''
+    for(let valor of datos){
+        table.innerHTML +=`<tr>
+                      <th scope="row">${valor.tipo}</th>
+                      <td>${valor.tarjeta}</td>
+                      <td>${valor.rendimiento}</td>
+                      <td>${valor.arquitectura}</td>
+                      <td>${valor.año}</td>
+                      <td>${valor.consumo}</td>
+                      <td>
+                        <div class="text-center">
+                          <div class="btn-group">
+                            <button class="btn btn-primary btnEditar">Editar</button>
+                            <button class="btn btn-danger btnBorrar">Borrar</button>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                    `
+    }
+}
