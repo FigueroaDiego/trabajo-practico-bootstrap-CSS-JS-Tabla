22ab9d15b3e0c70fd9709b3fddbc6b42961c1ffb
diego_07 <diego_33_04@hotmail.com>
2020-12-17 13:43:24 -0300

    modificacion tabla

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 let table = document.getElementById("tablaBody");
 let btnNuevo = document.getElementById("btnNuevo");
 
@@ -37,23 +36,7 @@ btnNuevo.addEventListener("click" , traer);
 
 
 $(document).ready(function(){ 
-  tablaProductos = $("#tablaProductos").DataTable({
-    "language": {
-      "lengthMenu": "Mostrar _MENU_ registros",
-      "zeroRecords": "No se encontraron resultados",
-      "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-      "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-      "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-      "sSearch": "Buscar:",
-      "oPaginate": {
-          "sFirst": "Primero",
-          "sLast":"Último",
-          "sNext":"Siguiente",
-          "sPrevious": "Anterior"
-       },
-       "sProcessing":"Procesando...",
-  }
-});
+  tablaProductos = $("#tablaProductos")
 
   
     
@@ -92,17 +75,11 @@ $(document).ready(function(){
     tipo = $(this).closest("tr").find('td:eq(0)').text();
     let respuesta = confirm("¿Está seguro de eliminar el registro: "+tipo+"?");
     if(respuesta){
-        $.ajax({
-            url: "/tabla.json",
-            type: "POST",
-            dataType: "json",
-            data: {opcion:opcion, tipo:tipo},
-            success: function(){
-              tablaProductos.row(fila.parents('tr')).remove().draw();
-            }
-        });
-    }   
+      tablaProductos.row(fila.parents('tr')).remove().draw();
+            }   
   });
+  
+  
   $("#formProductos").submit(function(e){
     e.preventDefault();    
     tipo = $.trim($("#tipo-producto").val());
@@ -111,23 +88,7 @@ $(document).ready(function(){
     arquitectura = $.trim($("#arquitectura-producto").val());
     año = $.trim($("#año-producto").val());
     consumo = $.trim($("#consumo-producto").val());
-    $.ajax({
-        url: "/tabla.json",
-        type: "POST",
-        dataType: "json",
-        data: {tipo:tipo, tarjeta:tarjeta, rendimiento:rendimiento, arquitectura:arquitectura, año:año, consumo:consumo},
-        success: function(data){  
-            console.log(data);
-            tipo = data[0].tipo;            
-            tarjeta = data[0].tarjeta;
-            rendimiento = data[0].rendimiento;
-            arquitectura = data[0].arquitectura;
-            año = data[0].año;
-            consumo = data[0].consumo;
-            if(opcion == 1){tablaProductos.row.add([tipo,tarjeta,rendimiento,arquitectura,año,consumo]).draw();}
-            else{tablaProductos.row(fila).data([tipo,tarjeta,rendimiento,arquitectura,año,consumo]).draw();}            
-        }        
-    });
+    
     $("#modalTable").modal("hide");    
   
   });    
