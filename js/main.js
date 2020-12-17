172c7aad56b05d18d23b75dff682d4966bedd3d6
diego_07 <diego_33_04@hotmail.com>
2020-12-16 21:27:50 -0300

    modificaciones

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
+
 let table = document.getElementById("tablaBody");
 let btnNuevo = document.getElementById("btnNuevo");
-
 
 function traer(){    
     fetch('/tabla.json')
@@ -14,7 +14,7 @@ function tabla(datos){
     table.innerHTML = ''
     for(let valor of datos){
         table.innerHTML +=`<tr>
-                      <th scope="row">${valor.tipo}</th>
+                      <td>${valor.tipo}</td>
                       <td>${valor.tarjeta}</td>
                       <td>${valor.rendimiento}</td>
                       <td>${valor.arquitectura}</td>
@@ -34,3 +34,108 @@ function tabla(datos){
 }
 
 btnNuevo.addEventListener("click" , traer);
+
+
+$(document).ready(function(){ 
+  tablaProductos = $("#tablaProductos").DataTable({
+    "language": {
+      "lengthMenu": "Mostrar _MENU_ registros",
+      "zeroRecords": "No se encontraron resultados",
+      "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+      "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+      "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+      "sSearch": "Buscar:",
+      "oPaginate": {
+          "sFirst": "Primero",
+          "sLast":"Último",
+          "sNext":"Siguiente",
+          "sPrevious": "Anterior"
+       },
+       "sProcessing":"Procesando...",
+  }
+});
+
+  
+    
+  
+  let fila; //capturar la fila para editar o borrar el registro
+    //botón EDITAR    
+  $(document).on("click",".btnEditar",function(){
+    fila = $(this).closest("tr");
+    tipo = fila.find('td:eq(0)').text();
+    tarjeta = fila.find('td:eq(1)').text();
+    rendimiento = parseInt(fila.find('td:eq(2)').text());
+    arquitectura = fila.find('td:eq(3)').text();
+    año = parseInt(fila.find('td:eq(4)').text());
+    consumo = fila.find('td:eq(5)').text();
+
+
+
+    
+    $("#tipo-producto").val(tipo);
+    $("#tarjeta-producto").val(tarjeta);
+    $("#rendimiento-producto").val(rendimiento);
+    $("#arquitectura-producto").val(arquitectura);
+    $("#año-producto").val(año);
+    $("#consumo-producto").val(consumo);
+    opcion = 2; //editar
+    
+    $(".modal-header").css("background-color", "#007bff");
+    $(".modal-header").css("color", "white");
+    $(".modal-title").text("Editar placa");            
+    $("#modalTable").modal("show");  
+    
+  });
+  
+  //botón BORRAR
+  $(document).on("click", ".btnBorrar", function(){    
+    fila = $(this);
+    tipo = $(this).closest("tr").find('td:eq(0)').text();
+    opcion = 3 //borrar
+    let respuesta = confirm("¿Está seguro de eliminar el registro: "+tipo+"?");
+    if(respuesta){
+        $.ajax({
+            url: "/tabla.json",
+            type: "POST",
+            dataType: "json",
+            data: {opcion:opcion, tipo:tipo},
+            success: function(){
+              tablaProductos.row(fila.parents('tr')).remove().draw();
+            }
+        });
+    }   
+  });
+  $("#formProductos").submit(function(e){
+    e.preventDefault();    
+    tipo = $.trim($("#tipo-producto").val());
+    tarjeta = $.trim($("#tarjeta-producto").val());
+    rendimiento = $.trim($("#rendimiento-producto").val());
+    arquitectura = $.trim($("#arquitectura-producto").val());
+    año = $.trim($("#año-producto").val());
+    consumo = $.trim($("#consumo-producto").val());
+    $.ajax({
+        url: "/tabla.json",
+        type: "POST",
+        dataType: "json",
+        data: {tipo:tipo, tarjeta:tarjeta, rendimiento:rendimiento, arquitectura:arquitectura, año:año, consumo:consumo},
+        success: function(data){  
+            console.log(data);
+            tipo = data[0].tipo;            
+            tarjeta = data[0].tarjeta;
+            rendimiento = data[0].rendimiento;
+            arquitectura = data[0].arquitectura;
+            año = data[0].año;
+            consumo = data[0].consumo;
+            if(opcion == 1){tablaProductos.row.add([tipo,tarjeta,rendimiento,arquitectura,año,consumo]).draw();}
+            else{tablaProductos.row(fila).data([tipo,tarjeta,rendimiento,arquitectura,año,consumo]).draw();}            
+        }        
+    });
+    $("#modalTable").modal("hide");    
+  
+  });    
+
+});    
+
+
+
+  
